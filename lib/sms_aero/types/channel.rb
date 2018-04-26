0d761e134b7cddabe91f9833b4631f5fd98ee314
Jaroslav Toropygin <jt@mesto.ru>
2018-04-26 19:04:11 +0300

    Fix channels

--- lib/sms_aero/types/channel.rb
+++ lib/sms_aero/types/channel.rb
@@ -1,4 +1,5 @@
 module SmsAero::Types
   # Describes acceptable channel codes
-  Channel = Coercible::Int.constrained included_in: [1, 2, 3, 4, 6]
+  # Channel = Coercible::Int.constrained included_in: [1, 2, 3, 4, 6]
+  Channel = Coercible::Int.constrained included_in: [4, 5, 6, 7, 8]
 end
